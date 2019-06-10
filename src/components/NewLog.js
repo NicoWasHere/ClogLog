51fa5b617b49557601b6692dee07366bdd6e9567
NicoWasHere <47311042+NicoWasHere@users.noreply.github.com>
2019-06-09 22:06:45 -0400

    Add files via upload

--- src/components/NewLog.js
+++ src/components/NewLog.js
@@ -2,13 +2,14 @@ import React, { useState }from "react"
 import DisplayAllLogs from "./DisplayAllLogs";
 
 export default props =>{
-
+if(window.localStorage){
 const [latestLog,getLog] = useState(localStorage.getItem(1)) //used to update the latest value
-
-
+}
 const buttonClicked = ()=>{
 const lastPoop = new Date().toLocaleDateString() + " "+new Date().toLocaleTimeString() //gets the date
+if(window.localStorage){
 localStorage.setItem(1,lastPoop) //adds it to local
+}
 getLog(lastPoop) //updates the latest on display
 }
 
