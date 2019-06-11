91b4b8f5df203f9b3e84d887eb6a8ccb0cfb664c
NicoWasHere <47311042+NicoWasHere@users.noreply.github.com>
2019-06-11 11:15:26 -0400

    Add files via upload

--- src/components/DisplayAllLogs.js
+++ src/components/DisplayAllLogs.js
@@ -4,13 +4,18 @@ import ClearLogs from "../components/ClearLogs"
 
 export default props =>{
   let finalDiv = [];
+  const [doTheStuff, setDoTheStuff] = useState(false)
 
-  if(typeof window !== undefined) {
-   
+  if(doTheStuff) {
     for(let i = 1; i<=localStorage.length; i++){
       finalDiv.unshift(<LogDisplay key = {i} logNumber = {i}/>)
     }
-  } 
+  }
+
+useEffect(()=>{
+  setDoTheStuff(true)
+},[])
+  
     return(
       <div>
       <div>{finalDiv}</div>
